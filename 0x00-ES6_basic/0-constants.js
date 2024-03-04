4a17d2d12153f2c025d130a73e47e59e02229e54
Reda Mjibel <reda_mjibel@hotmail.com>
2024-03-04 23:25:38 +0000

    update

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,4 +1,3 @@
-// Modified taskFirst to use const for variable instantiation
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
@@ -8,7 +7,6 @@ export function getLast() {
   return ' is okay';
 }
 
-// Modified taskNext to use let for variable instantiation
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
