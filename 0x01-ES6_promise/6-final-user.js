4c8f7cd6ecce8def3548d726180a1325477ee888
Reda Mjibel <reda_mjibel@hotmail.com>
2024-03-08 23:15:23 +0000

    update

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,17 +1,21 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   const res = [];
+
   try {
     const user = await signUpUser(firstName, lastName);
+
     res.push({ status: 'fulfilled', value: user });
+
     await uploadPhoto(fileName);
   } catch (err) {
-    res.push(({
+    res.push({
       status: 'rejected',
-      value: `Error: ${fileName} cannot be processed`,
-    }
+      value: `Error: ${fileName} cannot be processed. Reason: ${err.message}`,
+    });
   }
+
   return res;
 }
