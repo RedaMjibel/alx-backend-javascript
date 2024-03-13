a4e67152bf4435922b5904b8ba0ea57204707acc
Reda Mjibel <reda_mjibel@hotmail.com>
2024-03-13 02:44:50 +0000

    view + data

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -6,6 +6,7 @@ export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const view = new Int8Array(buffer);
   view[position] = value;
+  const dataview = new DataView(buffer);
 
-  return view;
+  return dataview;
 }
