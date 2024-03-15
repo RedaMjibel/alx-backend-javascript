b5347e55806e58f98000576331fede994ba91c3b
Reda Mjibel <reda_mjibel@hotmail.com>
2024-03-15 21:27:21 +0000

    empty string

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   if (!startString || !startString.length) {
-    return '';
+    return ('');
   }
   const filteredValues = [...set].filter((value) => value.startsWith(startString));
   const substrings = filteredValues.map((value) => value.substring(startString.length));
