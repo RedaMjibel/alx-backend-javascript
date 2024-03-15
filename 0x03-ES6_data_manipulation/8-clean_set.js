eb62a7239ef39b4297f61d21033b8fc4d1f868ca
Reda Mjibel <reda_mjibel@hotmail.com>
2024-03-15 21:17:01 +0000

    if start is empty

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,7 @@
 export default function cleanSet(set, startString) {
+  if (startString === '') {
+    return '';
+  }
   const filteredValues = [...set].filter((value) => value.startsWith(startString));
   const substrings = filteredValues.map((value) => value.substring(startString.length));
   return substrings.join('-');
